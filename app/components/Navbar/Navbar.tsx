9ca7a4ad6b480aa1dadded65137975870489f74e
ABNER RIVERA <abner500edrey@gmail.com>
2025-03-03 14:51:31 -0500

    feat: agregar nuevos estilos y configuración en Navbar para mostrar información de usuario logueado
    - Se añadieron estilos personalizados para mostrar el nombre, avatar y estado del usuario logueado.
    - Se configuró la lógica para mostrar/ocultar la información del usuario según su estado de autenticación.
    - Se mejoró la estructura del Navbar para una mejor integración de los nuevos elementos.

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,58 +3,102 @@ import { settings } from '@/config/settings';
 import styles from './Navbar.module.css';
 import Link from 'next/link';
 import { auth, signOut, signIn } from '@/auth';
+import Button from '../Button/Button';
+import Image from 'next/image';
 
 const Navbar = async () => {
-  const session = await auth();
+	const session = await auth();
 
-  return (
-    <div className={styles['navbar']}>
-      <h2>{settings.texts.siteName}</h2>
-      <div className={styles.navbar__divider}></div>
-      <ul className={styles.navbar__options}>
-        {settings.menuItems.map(({ name, path, icon }, index) => (
-          <li key={index} className={`${styles.navbar__link} ${styles.active}`}>
-            <Link href={path}>
-              {' '}
-              <span className={styles.navbar__icon}>{icon}</span> {name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div>
-        {session && session.user ? (
-          <>
-            <Link href="/startup/create">
-              <span>Create</span>
-            </Link>
+	return (
+		<div className={styles['navbar']}>
+			<h3>{settings.texts.siteName}</h3>
+			<div className={styles.navbar__divider}></div>
+			{/* navb options */}
+			<ul className={styles.navbar__options}>
+				{settings.menuItems.map(({ name, path, icon }, index) => (
+					<li key={index} className={`${styles.navbar__link} ${styles.active}`}>
+						<Link className="text-sm bold" href={path}>
+							{' '}
+							<span className={styles.navbar__icon}>{icon}</span> {name}
+						</Link>
+					</li>
+				))}
+			</ul>
+			{/* account pages */}
 
-            <form
-              action={async () => {
-                'use server';
-                await signOut({ redirectTo: '/' });
-              }}
-            >
-              <button type="submit">Logout</button>
-            </form>
+			{session && session.user ? (
+				<>
+					<h5>ACCOUNT PAGES</h5>
+					<ul className={styles.navbar__options}>
+						{settings.accountPages.map(({ name, path, icon }, index) => (
+							<li
+								key={index}
+								className={`${styles.navbar__link} ${styles.active}`}
+							>
+								<Link className="text-sm bold" href={path}>
+									{' '}
+									<span className={styles.navbar__icon}>{icon}</span> {name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</>
+			) : (
+				''
+			)}
 
-            {/* Usa `session.user.id` y `session.user.name` */}
-            <Link href={`/user/${session.user.name}`}>
-              <span>{session.user.name}</span>
-            </Link>
-          </>
-        ) : (
-          <form
-            action={async () => {
-              'use server';
-              await signIn('github');
-            }}
-          >
-            <button className={`btn`} type="submit">Login</button>
-          </form>
-        )}
-      </div>
-    </div>
-  );
+			{/* login - logout */}
+			<div className={styles.navbar__userCont}>
+				{session && session.user ? (
+					<>
+						<Image
+							className={styles.navbar__userImage}
+							src={`${session.user.image}`}
+							alt="userImage"
+							width={35}
+							height={35}
+						/>
+
+						{/* Usa `session.user.id` y `session.user.name` */}
+						<Link href={`/user/${session.user.name}`}>
+							<span className="bold">{session.user.name}</span>
+						</Link>
+
+						<Link href="/startup/create">
+							<span className="text-sm">Create HV</span>
+						</Link>
+
+						<form
+							action={async () => {
+								'use server';
+								await signOut({ redirectTo: '/' });
+							}}
+						>
+							<Button width="btnFull" variable="secondary">
+								Logout
+							</Button>
+						</form>
+					</>
+				) : (
+					<form
+						action={async () => {
+							'use server';
+							await signIn('github');
+						}}
+					>
+						<p className="text-sm">
+							Inicia sesion para poder subir tu hoja de vida y ver vacantes
+							disponibles
+						</p>
+						<br />
+						<Button type="submit" width="btnFull" variable="secondary">
+							Login
+						</Button>
+					</form>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
