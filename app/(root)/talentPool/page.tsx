c32489417f461bdac6c5fa692e03682a6cb9f312
ABNER RIVERA <abner500edrey@gmail.com>
2025-03-17 19:57:06 -0500

    feat: se agrega nueva manera de caprutar datos con el actual typado de user, y cambio de estilos

--- app/(root)/talentPool/page.tsx
+++ app/(root)/talentPool/page.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { supabase } from '@/lib/supabase'
+import { Session } from 'next-auth'
+import styles from './talentPool.module.css'
+import CardTalent from '@/app/components/CardTalent/CardTalent'
+
+type User = Session['user']
 
 const TalentPool = () => {
+
+  const [users, setUsers] = useState<User[]>([])
+
+  const fetchUsers = async () => {
+    const { data, error } = await supabase.from('users').select("*");
+  
+    if (error) {
+      console.log('Error al obtener usuarios:', error);
+    } else {
+      console.log('Usuarios obtenidos correctamente');
+  
+      // Normalizar los datos antes de guardarlos
+      const normalizedUsers: User[] = data.map(user => ({
+        ...user,
+        image: user.avatar_url, // Mapeo avatar_url -> image
+      }));
+  
+      setUsers(normalizedUsers);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+  
+  console.log(users)
+
   return (
-    <div>TalentPool</div>
+    <section>
+      <div className={styles.talentCont}>
+        {
+          users.map((item,index)=>(
+            <CardTalent key={index} user={item}/>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
