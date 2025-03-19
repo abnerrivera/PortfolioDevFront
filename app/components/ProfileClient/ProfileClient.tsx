ba98231990e6d5dca6719773670c02c2804e7b09
ABNER RIVERA <abner500edrey@gmail.com>
2025-03-18 21:56:32 -0500

    fix(feat): corregir error al traer datos y nueva funcionalidad de formulario con elementos custom

--- app/components/ProfileClient/ProfileClient.tsx
+++ app/components/ProfileClient/ProfileClient.tsx
@@ -1,153 +1,192 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { Session } from "next-auth";
-import Image from "next/image";
-import { supabase } from "@/lib/supabase";
-import styles from "./ProfileClient.module.css";
+'use client';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Session } from 'next-auth';
+import Image from 'next/image';
+import { supabase } from '@/lib/supabase';
+import styles from './ProfileClient.module.css';
+import Button from '../Button/Button';
+import CustomInput from '../CustomInput/CustomInput';
 
 interface ProfileClientProps {
-  session: Session | null;
+	session: Session | null;
 }
 
 interface FormData {
-  email: string;
-  name: string;
-  avatar_url: string;
-  profession: string;
-  experience: string;
-  projects: string;
-  skills: string;
-  age: string;
-  full_name: string;
+	email: string;
+	name: string;
+	avatar_url: string;
+	profession: string;
+	experience: string;
+	projects: string;
+	skills: string;
+	age: string;
+	full_name: string;
 }
 
 const ProfileClient = ({ session }: ProfileClientProps) => {
-  if (!session) return <p>No estás autenticado</p>;
+	if (!session) return <p>No estás autenticado</p>;
 
-  const { register, handleSubmit, reset, formState: { errors, isSubmitting } } = useForm<FormData>();
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors, isSubmitting },
+	} = useForm<FormData>();
 
-  const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      if (!session?.user.email) return;
-      setLoading(true);
+	useEffect(() => {
+		const fetchUserData = async () => {
+			if (!session?.user.email) return;
+			setLoading(true);
 
-      const { data, error } = await supabase
-        .from("users")
-        .select("*")
-        .eq("email", session.user.email)
-        .single();
+			const { data, error } = await supabase
+				.from('users')
+				.select('*')
+				.eq('email', session.user.email)
+				.single();
 
-      if (error) {
-        console.error("Error obteniendo datos:", error);
-      } else if (data) {
-        reset({
-          email: data.email || "",
-          name: data.name || "",
-          avatar_url: data.avatar_url || "",
-          profession: data.profession || "",
-          experience: Array.isArray(data.experience) ? data.experience.join(", ") : "",
-          projects: Array.isArray(data.projects) ? data.projects.join(", ") : "",
-          skills: Array.isArray(data.skills) ? data.skills.join(", ") : "",
-          age: data.age?.toString() || "",
-          full_name: data.full_name || "",
-        });
-      }
+			if (error) {
+				console.error('Error obteniendo datos:', error);
+			} else if (data) {
+				reset({
+					email: data.email || '',
+					name: data.name || '',
+					avatar_url: data.avatar_url || '',
+					profession: data.profession || '',
+					experience: Array.isArray(data.experience)
+						? data.experience.join(', ')
+						: '',
+					projects: Array.isArray(data.projects)
+						? data.projects.join(', ')
+						: '',
+					skills: Array.isArray(data.skills) ? data.skills.join(', ') : '',
+					age: data.age?.toString() || '',
+					full_name: data.full_name || '',
+				});
+			}
 
-      setLoading(false);
-    };
+			setLoading(false);
+		};
 
-    fetchUserData();
-  }, [session, reset]);
+		fetchUserData();
+	}, [session, reset]);
 
-  const onSubmit = async (data: FormData) => {
-    const { error } = await supabase
-      .from("users")
-      .update({
-        email: data.email,
-        name: data.name,
-        avatar_url: data.avatar_url,
-        profession: data.profession,
-        experience: data.experience.split(",").map((item) => item.trim()),
-        projects: data.projects.split(",").map((item) => item.trim()),
-        skills: data.skills.split(",").map((item) => item.trim()),
-        age: Number(data.age),
-        full_name: data.full_name,
-      })
-      .eq("email", session.user.email);
+	const onSubmit = async (data: FormData) => {
+		const { error } = await supabase
+			.from('users')
+			.update({
+				email: data.email,
+				name: data.name,
+				avatar_url: data.avatar_url,
+				profession: data.profession,
+				experience: data.experience.split(',').map((item) => item.trim()),
+				projects: data.projects.split(',').map((item) => item.trim()),
+				skills: data.skills.split(',').map((item) => item.trim()),
+				age: Number(data.age),
+				full_name: data.full_name,
+			})
+			.eq('email', session.user.email);
 
-    if (error) {
-      console.error("Error actualizando usuario:", error);
-    } else {
-      alert("Datos actualizados correctamente.");
-    }
-  };
+		if (error) {
+			console.error('Error actualizando usuario:', error);
+		} else {
+			alert('Datos actualizados correctamente.');
+		}
+	};
 
-  return (
-    <div className={styles.profileCont}>
-      <h1>Bienvenido, {session.user.name}</h1>
-      <Image width={100} height={100} src={session.user.image!} alt="Avatar" />
+	return (
+		<div className={styles.profileCont}>
+			<div className={styles.profileFormCont}>
+				{loading ? (
+					<p>Cargando datos...</p>
+				) : (
+					<form
+						className={styles.formProfile}
+						onSubmit={handleSubmit(onSubmit)}
+					>
+						<h1>Bienvenido, {session.user.name}</h1>
+						<div className={styles.formTwoInputs}>
+							<Image
+								width={100}
+								height={100}
+								src={session.user.image!}
+								alt="Avatar"
+							/>
+							<CustomInput
+								label="Avatar URL"
+								name="avatar_url"
+								type="text"
+								register={register}
+							/>
+						</div>
 
-      {loading ? (
-        <p>Cargando datos...</p>
-      ) : (
-        <form className={styles.formProfile} onSubmit={handleSubmit(onSubmit)}>
-          <label>
-            Email:
-            <input type="email" {...register("email", { required: true })} />
-            {errors.email && <span>El email es obligatorio</span>}
-          </label>
+						<div className={styles.formTwoInputs}>
+							<CustomInput
+								label="Nombre"
+								name="name"
+								type="text"
+								register={register}
+								error={errors.name}
+							/>
+							<CustomInput
+								label="Nombre Completo"
+								name="full_name"
+								type="text"
+								register={register}
+							/>
+						</div>
 
-          <label>
-            Nombre:
-            <input type="text" {...register("name", { required: true })} />
-            {errors.name && <span>El nombre es obligatorio</span>}
-          </label>
+						<div className={styles.formTwoInputs}>
+							<CustomInput
+								label="Email"
+								name="email"
+								type="email"
+								register={register}
+								error={errors.email}
+							/>
+							<CustomInput
+								label="Edad"
+								name="age"
+								type="number"
+								register={register}
+							/>
+						</div>
 
-          <label>
-            Avatar URL:
-            <input type="text" {...register("avatar_url")} />
-          </label>
+						<CustomInput
+							label="Profesión"
+							name="profession"
+							type="text"
+							register={register}
+						/>
+						<CustomInput
+							label="Experiencia (separada por comas)"
+							name="experience"
+							type="textarea"
+							register={register}
+						/>
+						<CustomInput
+							label="Proyectos (separados por comas)"
+							name="projects"
+							type="textarea"
+							register={register}
+						/>
+						<CustomInput
+							label="Habilidades (separadas por comas)"
+							name="skills"
+							type="textarea"
+							register={register}
+						/>
 
-          <label>
-            Profesión:
-            <input type="text" {...register("profession")} />
-          </label>
-
-          <label>
-            Experiencia (separada por comas):
-            <textarea {...register("experience")} />
-          </label>
-
-          <label>
-            Proyectos (separados por comas):
-            <textarea {...register("projects")} />
-          </label>
-
-          <label>
-            Habilidades (separadas por comas):
-            <textarea {...register("skills")} />
-          </label>
-
-          <label>
-            Edad:
-            <input type="number" {...register("age")} />
-          </label>
-
-          <label>
-            Nombre Completo:
-            <input type="text" {...register("full_name")} />
-          </label>
-
-          <button type="submit" disabled={isSubmitting}>
-            {isSubmitting ? "Guardando..." : "Actualizar"}
-          </button>
-        </form>
-      )}
-    </div>
-  );
+						<Button type="submit" disabled={isSubmitting}>
+							{isSubmitting ? 'Guardando...' : 'Actualizar'}
+						</Button>
+					</form>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default ProfileClient;
