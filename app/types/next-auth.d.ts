c2517861ce0dda61176d269b55f4d91ecee3657f
ABNER RIVERA <abner500edrey@gmail.com>
2025-03-23 11:15:11 -0500

    feat: nuevos tipados para los nuevos datos de la bd

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -2,20 +2,22 @@
 import 'next-auth';
 
 declare module 'next-auth' {
-  interface Session {
-    user: {
-      id: string; // Agrega `id` si es necesario
-      name?: string; // Agrega `name`
-      email?: string;
-      image?: string;
-      profession?: string;
-      years_experience?: number;
-      portfolio?: string;
-      full_name?: string;
-      experience?: string[];
-      projects?: string[];
-      skills?: string[];
-      age?: number;
-    };
-  }
+	interface Session {
+		user: {
+			id: string; // Agrega `id` si es necesario
+			name?: string; // Agrega `name`
+			email?: string;
+			image?: string;
+			profession?: string;
+			time_experience?: number;
+			time_unit?: number;
+			portfolio?: string;
+			full_name?: string;
+			experience?: string[];
+			projects?: string[];
+			skills?: string[];
+			main_skill?: string;
+			age?: number;
+		};
+	}
 }
