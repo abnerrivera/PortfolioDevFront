246c0b9fc61253bdd819cd383092e758189a8dc8
ABNER RIVERA <abner500edrey@gmail.com>
2025-04-05 12:53:08 -0500

    feat: crear filtro para mostrar solo usaurios con la info completa

--- app/(root)/talentPool/page.tsx
+++ app/(root)/talentPool/page.tsx
@@ -1,50 +1,51 @@
-'use client'
+'use client';
 
-import React, { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabase'
-import { Session } from 'next-auth'
-import styles from './talentPool.module.css'
-import CardTalent from '@/app/components/CardTalent/CardTalent'
+import React, { useEffect, useState } from 'react';
+import { supabase } from '@/lib/supabase';
+import { Session } from 'next-auth';
+import styles from './talentPool.module.css';
+import CardTalent from '@/app/components/CardTalent/CardTalent';
 
-type User = Session['user']
+type User = Session['user'];
 
 const TalentPool = () => {
+	const [users, setUsers] = useState<User[]>([]);
 
-  const [users, setUsers] = useState<User[]>([])
+	const fetchUsers = async () => {
+		const { data, error } = await supabase.from('users').select('*');
 
-  const fetchUsers = async () => {
-    const { data, error } = await supabase.from('users').select("*");
-  
-    if (error) {
-      console.log('Error al obtener usuarios:', error);
-    } else {
-      console.log('Usuarios obtenidos correctamente');
-  
-      // Normalizar los datos antes de guardarlos
-      const normalizedUsers: User[] = data.map(user => ({
-        ...user,
-        image: user.avatar_url, // Mapeo avatar_url -> image
-      }));
-  
-      setUsers(normalizedUsers);
-    }
-  };
+		if (error) {
+			console.log('Error al obtener usuarios:', error);
+		} else {
+			console.log('Usuarios obtenidos correctamente');
 
-  useEffect(() => {
-    fetchUsers()
-  }, [])
-  
-  return (
-    <section>
-      <div className={styles.talentCont}>
-        {
-          users.map((item,index)=>(
-            <CardTalent key={index} user={item}/>
-          ))
-        }
-      </div>
-    </section>
-  )
-}
+			// Normalizar los datos antes de guardarlos
+			const normalizedUsers: User[] = data.map((user) => ({
+				...user,
+				image: user.avatar_url, // Mapeo avatar_url -> image
+			}));
 
-export default TalentPool
+			setUsers(normalizedUsers);
+		}
+	};
+
+	const completedUser = users.filter(
+		(user) => user.full_name !== null && user.full_name?.trim() !== ''
+	);
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	return (
+		<section>
+			<div className={styles.talentCont}>
+				{completedUser.map((item, index) => (
+					<CardTalent key={index} user={item} />
+				))}
+			</div>
+		</section>
+	);
+};
+
+export default TalentPool;
