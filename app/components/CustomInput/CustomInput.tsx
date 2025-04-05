f173f47ff5e8a3d88bb30445f5b8394a0195916f
ABNER RIVERA <abner500edrey@gmail.com>
2025-04-05 12:29:40 -0500

    feat: agregar validaciones con zod

--- app/components/CustomInput/CustomInput.tsx
+++ app/components/CustomInput/CustomInput.tsx
@@ -2,44 +2,35 @@
 import { FC } from 'react';
 import { UseFormRegister, FieldError } from 'react-hook-form';
 import styles from './CustomInput.module.css';
+import { z } from 'zod';
+import { profileSchema } from '@/zod/validationSchema';
+import { MdOutlineError } from 'react-icons/md';
 
 // Define la interfaz del formulario
-interface FormData {
-	email: string;
-	name: string;
-	avatar_url: string;
-	profession: string;
-	time_experience: number;
-	time_unit: number;
-	portfolio: string;
-	experience: string;
-	projects: string;
-	main_skill: string;
-	skills: string;
-	age: string;
-	full_name: string;
-}
+type FormData = z.infer<typeof profileSchema>;
 
 interface CustomInputProps {
 	label?: string;
 	name: keyof FormData; // Solo permite claves de FormData
-	type?: 'text' | 'email' | 'number' | 'textarea' | 'select';
+	type?: 'text' | 'email' | 'number' | 'textarea' | 'select' | 'url';
 	placeholder?: string;
 	register: UseFormRegister<FormData>; // Usa la misma interfaz de useForm
 	error?: FieldError;
 	disabled?: boolean;
 	options?: { value: string; label: string }[];
+	maxLength?: number;
 }
 
 const CustomInput: FC<CustomInputProps> = ({
 	label,
 	name,
-	type = 'text',
+	type = '',
 	placeholder = '',
 	register,
 	error,
 	disabled,
 	options,
+	maxLength,
 }) => {
 	return (
 		<div className={styles.inputContainer}>
@@ -53,7 +44,7 @@ const CustomInput: FC<CustomInputProps> = ({
 					/>
 				) : type === 'select' && options ? (
 					<select
-						className={styles.input}
+						className={`${styles.input} ${styles.select}`}
 						{...register(name)}
 						disabled={disabled}
 					>
@@ -66,14 +57,34 @@ const CustomInput: FC<CustomInputProps> = ({
 				) : (
 					<input
 						disabled={disabled}
-						className={styles.input}
+						className={`${styles.input} ${error ? styles.inputError : ''}`}
 						type={type}
+						maxLength={maxLength}
 						{...register(name)}
 						placeholder={placeholder}
+						aria-invalid={!!error}
+						onInput={
+							type === 'text'
+								? (e) => {
+										const input = e.target as HTMLInputElement; // 🔹 Cast para acceder a `value`
+										input.value = input.value.replace(
+											/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/g,
+											''
+										);
+								  }
+								: undefined
+						}
 					/>
 				)}
 			</label>
-			{error && <span className={styles.error}>{error.message}</span>}
+			{error && (
+				<div className={styles.error}>
+					<div className={styles.error__icon}>
+						<MdOutlineError />
+					</div>
+					<div className={styles.error__msg}>{error.message}</div>
+				</div>
+			)}
 		</div>
 	);
 };
