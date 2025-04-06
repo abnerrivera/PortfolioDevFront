570b6f1ed8bf63de6a571c0428aadca6c37e686a
ABNER RIVERA <abner500edrey@gmail.com>
2025-04-06 16:05:06 -0500

    feat: cambiar estilos de fuentes y orden en formulario

--- app/components/CustomInput/CustomInput.tsx
+++ app/components/CustomInput/CustomInput.tsx
@@ -11,10 +11,10 @@ type FormData = z.infer<typeof profileSchema>;
 
 interface CustomInputProps {
 	label?: string;
-	name: keyof FormData; // Solo permite claves de FormData
+	name: keyof FormData;
 	type?: 'text' | 'email' | 'number' | 'textarea' | 'select' | 'url';
 	placeholder?: string;
-	register: UseFormRegister<FormData>; // Usa la misma interfaz de useForm
+	register: UseFormRegister<FormData>;
 	error?: FieldError;
 	disabled?: boolean;
 	options?: { value: string; label: string }[];
@@ -34,55 +34,57 @@ const CustomInput: FC<CustomInputProps> = ({
 }) => {
 	return (
 		<div className={styles.inputContainer}>
-			<label className={styles.label}>
-				{label}
-				{type === 'textarea' ? (
-					<textarea
-						className={styles.input}
-						{...register(name)}
-						placeholder={placeholder}
-					/>
-				) : type === 'select' && options ? (
-					<select
-						className={`${styles.input} ${styles.select}`}
-						{...register(name)}
-						disabled={disabled}
-					>
-						{options.map((option) => (
-							<option key={option.value} value={option.value}>
-								{option.label}
-							</option>
-						))}
-					</select>
-				) : (
-					<input
-						disabled={disabled}
-						className={`${styles.input} ${error ? styles.inputError : ''}`}
-						type={type}
-						maxLength={maxLength}
-						{...register(name)}
-						placeholder={placeholder}
-						aria-invalid={!!error}
-						onInput={
-							type === 'text'
-								? (e) => {
-										const input = e.target as HTMLInputElement; // 🔹 Cast para acceder a `value`
-										input.value = input.value.replace(
-											/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/g,
-											''
-										);
-								  }
-								: undefined
-						}
-					/>
-				)}
-			</label>
+			{label && <label className={styles.label}>{label}</label>}
+			{type === 'textarea' ? (
+				<textarea
+					className={`${styles.input} ${error ? styles.inputError : ''}`}
+					{...register(name)}
+					placeholder={placeholder}
+					disabled={disabled}
+				/>
+			) : type === 'select' && options ? (
+				<select
+					className={`${styles.input} ${styles.select} ${
+						error ? styles.inputError : ''
+					}`}
+					{...register(name)}
+					disabled={disabled}
+				>
+					{options.map((option) => (
+						<option key={option.value} value={option.value}>
+							{option.label}
+						</option>
+					))}
+				</select>
+			) : (
+				<input
+					disabled={disabled}
+					className={`${styles.input} ${error ? styles.inputError : ''}`}
+					type={type}
+					maxLength={maxLength}
+					{...register(name)}
+					placeholder={placeholder}
+					aria-invalid={!!error}
+					onInput={
+						type === 'text'
+							? (e) => {
+									const input = e.target as HTMLInputElement;
+									input.value = input.value.replace(
+										/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/g,
+										''
+									);
+							  }
+							: undefined
+					}
+				/>
+			)}
+
 			{error && (
 				<div className={styles.error}>
 					<div className={styles.error__icon}>
 						<MdOutlineError />
+						<span className={styles.error__msg}>{error.message}</span>
 					</div>
-					<div className={styles.error__msg}>{error.message}</div>
 				</div>
 			)}
 		</div>
