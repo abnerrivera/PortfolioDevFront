6dfec67a0301be0988612ff226981effeaae7ce6
ABNER RIVERA <abner500edrey@gmail.com>
2025-04-17 23:52:46 -0500

    🔧 Update: 📝 BannerHome.module.css, 📝 BannerHome.tsx, 📝 globals.css,
    Las modificaciones mejoran la funcionalidad y el estilo del componente `BannerHome`. Se añadió la directiva `'use client'` para habilitar el uso de React Server Components.  Se integró un componente `ExperienceTab` y se implementó un nuevo estilo de borde (`globals.css`).  Finalmente, se añadieron datos de proyecto a `settings.tsx`, presumiblemente para su uso en el componente `BannerHome`.

--- app/components/BannerHome/BannerHome.tsx
+++ app/components/BannerHome/BannerHome.tsx
@@ -1,40 +1,76 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import stylesHeader from './BannerHome.module.css';
 import Image from 'next/image';
 import profilePhoto from '../../assets/profile.png';
 import { settings } from '@/config/settings';
+import ExperienceTab from '../ExperienceTab/ExperienceTab';
+import ProjectsTab from '../ProjectsTab/ProjectsTab';
+import SkillsTab from '../SkillsTab/SkillsTab';
+import { profileData } from '@/config/settings';
 
 const BannerHome = () => {
-	return (
-		<div className={stylesHeader.header}>
-			<div className={stylesHeader.header__up}></div>
-			<div className={stylesHeader.header__down}>
-				<div className={stylesHeader.header__photo}>
-					<Image
-						src={profilePhoto}
-						alt="profile photo"
-						width={100}
-						height={100}
-					/>
+	const [activeTab, setActiveTab] = useState<string>(
+		settings.headerOptions[0].name || 'default'
+	);
 
-					<div className={stylesHeader.header__info}>
-						<p className="bold">{settings.texts.siteName}</p>
-						<p className="txt-gray bold">{settings.texts.email}</p>
+	// Mapeo de los nombres de las pestañas a sus componentes de contenido
+	const tabContentMap: { [key: string]: () => React.ReactNode } = {
+		projects: () => (
+			<div className={stylesHeader.tabContItems}>
+				{profileData.projects.map((item, index) => (
+					<ProjectsTab key={index} {...item} />
+				))}
+			</div>
+		),
+		experience: ExperienceTab,
+		skills: SkillsTab,
+		// Agrega aquí más mapeos según tus opciones de encabezado
+	};
+
+	// Obtener el componente de contenido activo basado en el estado
+	const ActiveTabContent =
+		tabContentMap[activeTab] || (() => <p>Contenido no encontrado</p>);
+
+	return (
+		<>
+			<div className={stylesHeader.header}>
+				<div className={stylesHeader.header__up}></div>
+				<div className={stylesHeader.header__down}>
+					<div className={stylesHeader.header__photo}>
+						<Image
+							src={profilePhoto}
+							alt="profile photo"
+							width={100}
+							height={100}
+						/>
+						<div className={stylesHeader.header__info}>
+							<p className="bold">{settings.texts.siteName}</p>
+							<p className="txt-gray bold">{settings.texts.email}</p>
+						</div>
+					</div>
+
+					<div className={stylesHeader.header__options}>
+						<ul>
+							{settings.headerOptions.map(({ icon, name }, index) => (
+								<li
+									key={index}
+									onClick={() => setActiveTab(name)}
+									className={activeTab === name ? stylesHeader.active : ''} // Clase activa para el estilo
+								>
+									{icon}
+									{name}
+								</li>
+							))}
+						</ul>
 					</div>
 				</div>
-
-				<div className={stylesHeader.header__options}>
-					<ul>
-						{settings.headerOptions.map(({ icon, name }, index) => (
-							<li key={index}>
-								{icon}
-								{name}
-							</li>
-						))}
-					</ul>
-				</div>
 			</div>
-		</div>
+			<div className={stylesHeader.tabCont}>
+				<ActiveTabContent />
+			</div>
+		</>
 	);
 };
 
---
+++ app/components/ExperienceTab/ExperienceTab.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import  styles  from "./ExperienceTab.module.css";
+
+const ExperienceTab = () => {
+  return (
+    <div>ExperienceTab</div>
+  )
+}
+
+export default ExperienceTab
